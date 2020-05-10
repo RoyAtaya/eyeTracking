c7a0f8b6cb4001ab338ff06b8047e007524417e3
ranyaataya <ranya.ataya@gmail.com>
2020-05-10 16:04:11 -0700

    changed a photo from BGR to grayscale

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
+/*
+##################################################
+					PLANNING
+##################################################
+NECESSARY FUNCTIONS:
+- main function
+- draws a bounding box around the eye and displays it in a separate windowm (output image)
+- function to get images from video feed
+- function to process input images
+- other helper functions
+
+OTHER NOTES:
+- need to determine a method of finding eye in image
+	- maybe ORB or SIFT (if available in c++ and free)
+- should we track both eyes? or just one eye?
+- how to tell if there is no eye?
+- could be done with machine learning
+- Should we use objects? I don't think we need objects...
+*/
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
 
 int main() {
-	Mat img = imread("deadAnya.png");
+	Mat img = imread("fatSquirttle.jpg");
+	Mat grayImg;
+	cvtColor(img, grayImg, COLOR_BGR2GRAY);
 	namedWindow("image", WINDOW_NORMAL);
-	imshow("image", img);
+	imshow("image", grayImg);
 	waitKey(0);
 	return 0;
 }
