07db11831be1279dc59e932b4c6e8e75aea459f8
ranyaataya <ranya.ataya@gmail.com>
2020-05-12 23:01:39 -0700

    added new header file for image manipulation functions

---
+++ imageManipulation.h
@@ -0,0 +1,2 @@
+// Functions
+void drawBoundingBox(cv::Mat img, int posX, int posY, int width, int height);
--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ OTHER NOTES:
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include "Util.h"
+#include "imageManipulation.h"
 using namespace std;
 using namespace cv;
 
@@ -42,24 +42,4 @@ int main() {
 	waitKey(0);
 	*/
 	return 0;
-}
-
-/*
-Purpose:	draws a bounding box (ellipse) on a given image with a provided position
-			and size
-Param:		img		-	image that we want to draw on
-			posX	-	position of the bounding ellipse in the x direction
-			posY	-	position of the boudning ellipse in the y direction
-			width	-	width of bounding ellipse
-			height	-	height of bounding ellipse
-*/
-void drawBoundingBox(Mat img, int posX, int posY, int width, int height) {
-	int thickness = 2;
-	int lineType = 8;
-	int angle = 0;
-	Scalar color = Scalar(0, 0, 255); // BGR
-
-	ellipse(img, Point(posX, posY), Size(width, height), angle, 0, 360, color, thickness, lineType);
-	imshow("image2", img);
-	waitKey();
 }
